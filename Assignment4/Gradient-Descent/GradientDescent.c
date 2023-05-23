4b9f98bb41f9708fdc0593a8060a0b5d5563be82
Silence <blue_silence@outlook.com>
2023-05-22 20:05:21 -0400

    GD finished.

--- Assignment4/Gradient-Descent/GradientDescent.c
+++ Assignment4/Gradient-Descent/GradientDescent.c
@@ -10,6 +10,7 @@ typedef struct {
 
 V getGradient(V p);
 V mulV(V p, V grad, mpfr_t t);
+V resultCalc(V p);
 
 int main (void)
 {
@@ -17,8 +18,8 @@ int main (void)
     V p;
     mpfr_init2(p.x1,200);
     mpfr_init2(p.x2,200);
-    mpfr_set_d (p.x1, 1.0, MPFR_RNDD);
-    mpfr_set_d (p.x2, 1.0, MPFR_RNDD);
+    mpfr_set_d (p.x1, 0, MPFR_RNDD);
+    mpfr_set_d (p.x2, 0, MPFR_RNDD);
 
     mpfr_t t;
     mpfr_init2 (t, 200);
@@ -32,15 +33,20 @@ int main (void)
         V g = getGradient(p);
         p = mulV(p, g , t);
     }
+    V result = resultCalc(p);
 
-    printf ("x1: ");
+    printf ("x1*= ");
     mpfr_out_str (stdout, 10, 0, p.x1, MPFR_RNDD);
-    printf ("      x2: ");
+    printf ("  x2*= ");
     mpfr_out_str (stdout, 10, 0, p.x2, MPFR_RNDD);
+    putchar ('\n');
+    printf ("f*= ");
+    mpfr_out_str (stdout, 10, 0, result.x1, MPFR_RNDD);
     putchar ('\n');
     mpfr_clear (p.x1);
     mpfr_clear (p.x2);
     mpfr_clear (t);
+    mpfr_clear (result.x1);
     mpfr_free_cache ();
     return 0;
 }
@@ -180,3 +186,69 @@ V mulV(V p, V grad, mpfr_t t) {
     mpfr_clear (grad.x2);
     return p;
 }
+
+V resultCalc(V p) {
+    V r;
+    mpfr_init2(r.x1,200);
+    mpfr_set_d (r.x1, 0, MPFR_RNDD);
+    {
+                mpfr_t v1, v2, v3, v4, a1,a2,a3;
+        mpfr_init2(v1,200);
+        mpfr_init2(v2,200);
+        mpfr_init2(v3,200);
+        mpfr_init2(v4,200);
+        mpfr_init2(a1,200);
+        mpfr_init2(a2,200);
+        mpfr_init2(a3,200);
+
+        mpfr_set_d (a1, M_E, MPFR_RNDD);
+        mpfr_set_d (a2, M_E, MPFR_RNDD);
+        mpfr_set_d (a3, M_E, MPFR_RNDD);
+
+        mpfr_set_d (v1, 0, MPFR_RNDD);
+        mpfr_set_d (v2, 3, MPFR_RNDD);
+        mpfr_set_d (v3, -0.1, MPFR_RNDD);
+        mpfr_set_d (v4, 0, MPFR_RNDD);
+
+        mpfr_mul(v2, v2, p.x2, MPFR_RNDD);
+        mpfr_add(v4, v2, v4, MPFR_RNDD);
+        mpfr_add(v4, p.x1, v4, MPFR_RNDD);
+        mpfr_add(v4, v3, v4, MPFR_RNDD);
+        mpfr_exp(a1, v4, MPFR_RNDD);
+
+
+        mpfr_set_d (v1, -3, MPFR_RNDD);
+        mpfr_set_d (v2, -0.1, MPFR_RNDD);
+        mpfr_set_d (v3, 0, MPFR_RNDD);
+        //mpfr_set_d (v4, 0, MPFR_RNDD);
+
+        mpfr_mul(v1, v1, p.x2, MPFR_RNDD);
+        mpfr_add(v3, v1, v3, MPFR_RNDD);
+        mpfr_add(v3, p.x1, v3, MPFR_RNDD);
+        mpfr_add(v3, v2, v3, MPFR_RNDD);
+        mpfr_exp(a2, v3, MPFR_RNDD);
+
+
+        mpfr_set_d (v1, -0.1, MPFR_RNDD);
+        mpfr_set_d (v2, 0, MPFR_RNDD);
+        mpfr_set_d (v3, 1, MPFR_RNDD);
+
+        mpfr_add(v2, v2, v1, MPFR_RNDD);
+        mpfr_sub(v2, v2, p.x1, MPFR_RNDD);
+        mpfr_exp(a3, v2, MPFR_RNDD);
+        mpfr_mul(a3, v3, a3, MPFR_RNDD);
+
+        mpfr_add(r.x1, a1, a2, MPFR_RNDD); 
+        mpfr_add(r.x1, r.x1, a3, MPFR_RNDD); 
+
+        mpfr_clear (v1);
+        mpfr_clear (v2);
+        mpfr_clear (v3);
+        mpfr_clear (v4);
+        mpfr_clear (a1);
+        mpfr_clear (a2);
+        mpfr_clear (a3);
+    }
+    return r;
+}
+
